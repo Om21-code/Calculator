ea8484770d7184c2120f5ff9ae67f72724a1888e
Om Agarwal <agarwalom2004@gmail.com>
2025-11-25 17:43:31 +0530

    Calculator

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-// Todo: Make M+ M- and MC functional
 let string = "";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const buttonValue = e.target.innerHTML;
+
+        if (buttonValue == '=') {
+            try {
+                
+                string = eval(string);
+            } catch (error) {
+                
+                string = "Error";
+            }
+            document.querySelector('input').value = string;
+
+        } else if (buttonValue == 'C') {
+            string = "";
+            document.querySelector('input').value = string;
+
+        } else if (buttonValue == '%') {
+            
+            try {
+               
+                let currentValue = eval(string);
+                string = String(currentValue / 100);
+            } catch (error) {
+                
+                string = "Error";
+            }
+            document.querySelector('input').value = string;
+
+        } else {
+            console.log(e.target);
+            string = string + buttonValue;
+            document.querySelector('input').value = string;
+        }
+    });
+});
